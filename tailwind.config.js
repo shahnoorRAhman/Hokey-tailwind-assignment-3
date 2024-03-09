fcc86f745dd070a2f1feb70652c44e2b0fc6ad06
shahnoorRAhman <mdshahnoor33@gmail.com>
2024-03-09 17:26:36 +0600

    Program Sections done with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+        extend: {
+            backgroundImage: {
+                'bg-image': "url('../Images/2.png')",
+              }
+      },
     },
     plugins: [],
   }
